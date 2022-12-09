84c367c23f3269e7b31a964e2f0522b544640ef8
Hayden Whitfield <haydenwhitfield267@gmail.com>
2022-12-08 19:25:51 -0500

    Added logic for start button and next question function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,40 @@
-var strtBttn = document.querySelector('#strtBttn');
+const startButton = document.getElementById('startButton')
+const questionContainerELement = document.getElementById('questionContainer')
 
-strtBttn.addEventListener('click', function() {
-    strtBttn.textContent = 'YOU FOOL!'
-    
-});
+let shuffledQuestions, currentQuestionIndex 
+
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answerButton')
+
+startButton.addEventListener('click', startGame)
+
+function startGame(){
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerELement.classList.remove('hide')
+    setNextQuestion();
+}
+
+function setNextQuestion(){
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
+
+function selectAnswer(){
+
+}
+
+const questions = [
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            {text: '4', correct: true},
+            {text: '22', correct: false}
+        ]
+
+    }
+]
